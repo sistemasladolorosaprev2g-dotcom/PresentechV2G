3403b2d6db2af402985c17f187f848f33cf5c041
Zapiony <sazapatac4@gmail.com>
2026-05-20 11:47:54 -0500

    Update credentials

--- src/frontend/src/services/api.js
+++ src/frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
 export const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: baseUrl,
   headers: {
     'Content-Type': 'application/json',
   },
